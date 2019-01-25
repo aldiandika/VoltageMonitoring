68c8a1cb74f2aaa6a0bea90451e1d5fae77e5338
aldiandika <aldi.andika@gmail.com>
2019-01-25 16:59:09 +0700

    Initial Commit

--- app/src/main/java/com/example/aldiandika/voltagemonitoring/util/DatabaseSettingHelper.java
+++ app/src/main/java/com/example/aldiandika/voltagemonitoring/util/DatabaseSettingHelper.java
@@ -1,4 +1,16 @@
 package com.example.aldiandika.voltagemonitoring.util;
 
 public class DatabaseSettingHelper {
+
+    private static final String DATABASE_NAME = "monitoringsqlite.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "tablemonitor";
+    private static final String COL_1 = "ID";
+    private static final String COL_2 = "VOLTAGE_1";
+    private static final String COL_3 = "VOLTAGE_2";
+    private static final String COL_4 = "VOLTAGE_3";
+    private static final String COL_5 = "VOLTAGE_4";
+    private static final String COL_6 = "ARUS";
+    private static final String COL_7 = "DAYA";
+    private static final String COL_8 = "CREATED_AT";
 }
